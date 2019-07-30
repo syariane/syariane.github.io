18d23dc8b44f5e35fb41d1cdf77f9a95d52395e8
Ariane Sy <sya@sheridancollege.ca>
2019-07-30 11:19:05 -0400

    Finished implementation.

---
+++ syaA3/assets/data/myBooks.ts
@@ -0,0 +1,32 @@
+import { Book } from "../../app/bookInfo";
+export const MYBOOKS: Book[] =
+[
+    {
+        bname: "Away With the Fairies",
+        bauthor: "Kerry GreenWood",
+        bgenre: "Mystery",
+        byear: "2001",
+        bpicture: "./assets/images/AwayWithTheFairies.jpg"
+    },
+    {
+        bname: "Death At Victoria Dock",
+        bauthor: "Kerry GreenWood",
+        bgenre: "Mystery",
+        byear: "2005",
+        bpicture: "./assets/images/DeathAtVictoriaDock.jpg"
+    },
+    {
+        bname: "Murder and Mendelssohn",
+        bauthor: "Kerry GreenWood",
+        bgenre: "Mystery",
+        byear: "2013",
+        bpicture: "./assets/images/MurderAndMendelssohn.jpg"
+    },
+    {
+        bname: "Murder in Montparnasse",
+        bauthor: "Kerry GreenWood",
+        bgenre: "Mystery",
+        byear: "2002",
+        bpicture: "./assets/images/MurderInMontparnasse.jpg"
+    }
+]
